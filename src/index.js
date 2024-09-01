454eb031232a24ba991f57712144fbab15c10a9f
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-09-01 22:40:25 +0700

    scss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const hbs = require('express-handlebars');
 const path = require('path');
 const app = express();
 const port = 3000;
+console.log(path.join(__dirname))
+app.use(express.static(path.join(__dirname,'public')));
 
 //http logger
 app.use(morgan('combined'));
@@ -13,7 +15,7 @@ app.engine('hbs', hbs.engine({
   extname: ".hbs"
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resourses/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
