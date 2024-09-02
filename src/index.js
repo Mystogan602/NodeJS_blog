53cf5e716f205cca042362bbe5d1cc527c4f6f96
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-09-02 18:22:05 +0700

    create routes, app/controllers

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ const hbs = require('express-handlebars');
 const path = require('path');
 const app = express();
 const port = 3000;
-console.log(path.join(__dirname))
+
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname,'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 //http logger
 app.use(morgan('combined'));
@@ -17,13 +24,8 @@ app.engine('hbs', hbs.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+//Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
