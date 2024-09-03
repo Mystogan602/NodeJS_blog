5381a0d8f1a3be9e80ece1e159d3d755e296d3a3
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-09-03 15:34:25 +0700

    add mongoose

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,16 @@
+const Course = require('../models/Course');
+
 class SiteController {
     //[GET]/
     index(req, res) {
-        res.render('home');
+        console.log(Course);
+        Course.find({})
+            .then((courses) => res.json(courses))
+            .catch((error) =>
+                res.status(400).json({ error: 'Course not found' }),
+            );
+
+        // res.render('home');
     }
     //[GET]/search
     search(req, res) {
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 const hbs = require('express-handlebars');
 const path = require('path');
-const app = express();
-const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+
+//connect to Database
+db.connect();
+
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,7 +33,7 @@ app.engine(
 );
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 //Route init
 route(app);
