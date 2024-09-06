e7fa55a725138bda7e98e493d1cd778a7ba6d756
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-09-06 14:03:28 +0700

    update name middleware

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const hbs = require('express-handlebars');
 const path = require('path');
 const methodOverride = require('method-override');
 
-const SortMiddleware = require('./app/middlewares/sortMiddlewaree');
+const SortMiddleware = require('./app/middlewares/sortMiddleware');
 
 const route = require('./routes');
 const db = require('./config/db');
